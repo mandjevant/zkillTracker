d29d9b1e0d8dd4d883acf429521d4f75005d9f6d
mandjevant <38689620+mandjevant@users.noreply.github.com>
2024-12-01 13:04:20 +0100

    Set dropdown title to default/chosen corp name

--- zkilltracker/src/App.js
+++ zkilltracker/src/App.js
@@ -3,19 +3,19 @@ import React, { useState, useEffect } from 'react';
 import ChooseCorpDropdown from './components/chooseCorpDropdown';
 
 function App() {
-  const [activeCorporation, setActiveCorporation] = useState(98753041);
-  const [corporationInfo, setCorporationInfo] = useState(0);
+  const [activeCorporationId, setActiveCorporationId] = useState(98753041);
+  const [activeCorporation, setActiveCorporation] = useState(false);
 
   useEffect(() => {
-    fetch("/corporation/" + String(activeCorporation)).then(res => res.json()).then(data => {
-      setCorporationInfo(data.name);
+    fetch("/corporation/" + String(activeCorporationId)).then(res => res.json()).then(data => {
+      setActiveCorporation(data);
     });
-  }, [activeCorporation]);
+  }, [activeCorporationId]);
 
   return (
     <div className="App">
-      <p>{corporationInfo}</p>
-      <ChooseCorpDropdown setActiveCorporation={setActiveCorporation} />
+      <p>{activeCorporation.name}</p>
+      <ChooseCorpDropdown activeCorporation={activeCorporation} setActiveCorporationId={setActiveCorporationId} />
     </div>
   );
 }
