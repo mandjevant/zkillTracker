b4fd506449b0697e765fa3fa7411b6d8e659b2ef
mandjevant <38689620+mandjevant@users.noreply.github.com>
2024-12-01 12:52:18 +0100

    style and add dropdown.

--- zkilltracker/src/App.js
+++ zkilltracker/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import ChooseCorpDropdown from './components/chooseCorpDropdown';
 
 function App() {
   const [activeCorporation, setActiveCorporation] = useState(98753041);
   const [corporationInfo, setCorporationInfo] = useState(0);
 
   useEffect(() => {
-      fetch("/corporation/" + String(activeCorporation)).then(res => res.json()).then(data => {
-        setCorporationInfo(data.name);
-      })
-    }
-  )
+    fetch("/corporation/" + String(activeCorporation)).then(res => res.json()).then(data => {
+      setCorporationInfo(data.name);
+    });
+  }, [activeCorporation]);
 
   return (
     <div className="App">
       <p>{corporationInfo}</p>
+      <ChooseCorpDropdown setActiveCorporation={setActiveCorporation} />
     </div>
   );
 }
