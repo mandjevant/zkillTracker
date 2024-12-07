23786df0f5b7ef0041612ad8b5d4fb3d3c5227dc
mandjevant <38689620+mandjevant@users.noreply.github.com>
2024-12-07 14:22:42 +0100

    update for charts

--- zkilltracker/src/App.js
+++ zkilltracker/src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CorporationView from './components/corporationView';
+import { MantineProvider } from '@mantine/core';
+import AllianceView from './components/allianceView';
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/charts/styles.css';
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<CorporationView />} />
-    </Routes>
+    <MantineProvider defaultColorScheme="dark">
+      <Routes>
+        <Route path="/" element={<CorporationView />} />
+        <Route path="/alliance" element={<AllianceView />} />
+      </Routes>
+    </MantineProvider>
   );
 }
 
