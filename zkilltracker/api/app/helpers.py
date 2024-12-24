e6b319697426bd5eb48a5d5a038415bb98eea490
mandjevant <38689620+mandjevant@users.noreply.github.com>
2024-12-24 14:02:09 +0100

    serialize objects

--- zkilltracker/api/app/helpers.py
+++ zkilltracker/api/app/helpers.py
@@ -15,8 +15,9 @@ def serialize_corporation(corp):
         "attackersLost": corp.attackersLost,
         "shipsDestroyed": corp.shipsDestroyed,
         "iskDestroyed": corp.iskDestroyed,
-        "attackersDestroyed": corp.attackersDestroyed
+        "attackersDestroyed": corp.attackersDestroyed,
     }
+
 
 def serialize_month(month):
     return {
@@ -28,8 +29,9 @@ def serialize_month(month):
         "iskLost": month.iskLost,
         "shipsDestroyed": month.shipsDestroyed,
         "pointsDestroyed": month.pointsDestroyed,
-        "iskDestroyed": month.iskDestroyed
+        "iskDestroyed": month.iskDestroyed,
     }
+
 
 def serialize_alliance(entry):
     return {
@@ -40,5 +42,63 @@ def serialize_alliance(entry):
         "killsPerActiveMain": entry.killsPerActiveMain,
         "percentageOfAllianceKills": entry.percentageOfAllianceKills,
         "year": entry.year,
-        "month": entry.month
+        "month": entry.month,
+    }
+
+
+def serialize_aggregations(aggs):
+    return {
+        "year_month": aggs.year_month,
+        "totalValue": aggs.totalValue,
+        "points": aggs.points,
+        "npc": aggs.npc,
+        "solo": aggs.solo,
+        "awox": aggs.awox,
+        "killCount": aggs.killCount,
+    }
+
+
+def serialize_member(member):
+    return {
+        "characterID": member.characterID,
+        "characterName": member.characterName,
+        "corporationID": member.corporationID,
+    }
+
+
+def serialize_member_kills(member_kill):
+    return {
+        "killID": member_kill.killID,
+        "attackerCharacterID": member_kill.characterID,
+        "damageDone": member_kill.damageDone,
+        "finalBlow": member_kill.finalBlow,
+        "attackerShipTypeID": member_kill.shipTypeID,
+    }
+
+
+def serialize_kills(kill):
+    return {
+        "killID": kill.killID,
+        "locationID": kill.locationID,
+        "totalValue": kill.totalValue,
+        "points": kill.points,
+        "npc": kill.npc,
+        "solo": kill.solo,
+        "awox": kill.awox,
+        "datetime": kill.datetime,
+        "shipTypeID": kill.shipTypeID,
+    }
+
+
+def serialize_items(item):
+    return {
+        "type_id": item.type_id,
+        "name": item.name,
+    }
+
+
+def serialize_month_progress(item):
+    return {
+        "characterName": item.characterName,
+        "kills": item.kills,
     }
