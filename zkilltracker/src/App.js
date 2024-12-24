cff33abffa376375a0ee45a4abf020b05be0b641
mandjevant <38689620+mandjevant@users.noreply.github.com>
2024-12-24 14:05:39 +0100

    more routes

--- zkilltracker/src/App.js
+++ zkilltracker/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import CorporationView from './components/corporationView';
 import { MantineProvider } from '@mantine/core';
+import CorporationView from './components/corporationView';
 import AllianceView from './components/allianceView';
+import MemberView from './components/memberView';
+import AdminView from './components/adminView';
+import { Notifications } from '@mantine/notifications';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/charts/styles.css';
@@ -12,9 +15,12 @@ import '@mantine/charts/styles.css';
 function App() {
   return (
     <MantineProvider defaultColorScheme="dark">
+      <Notifications limit={5} zIndex={1000} />
       <Routes>
         <Route path="/" element={<CorporationView />} />
         <Route path="/alliance" element={<AllianceView />} />
+        <Route path="/members" element={<MemberView />} />
+        <Route path="/admin" element={<AdminView />} />
       </Routes>
     </MantineProvider>
   );
