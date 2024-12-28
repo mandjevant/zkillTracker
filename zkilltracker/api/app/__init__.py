c46d562bfda0a18398bbf6f39424820f4523b113
mandjevant <38689620+mandjevant@users.noreply.github.com>
2024-12-28 19:10:15 +0100

    prepare websocket

--- zkilltracker/api/app/__init__.py
+++ zkilltracker/api/app/__init__.py
@@ -1,13 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
+import uuid
+from flask_socketio import SocketIO
 
 
 db = SQLAlchemy()
 app = Flask(__name__)
 db_name = "zkillboard_stats.db"
+app.secret_key = str(uuid.uuid4())
+socketio = SocketIO(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_name
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+EVE_CLIENT_ID = environ.get("CLIENT_ID")
+EVE_CLIENT_SECRET = environ.get("SECRET_KEY")
+EVE_REDIRECT_URI = environ.get("CALLBACK_URL")
+EVE_AUTH_URL = "https://login.eveonline.com/v2/oauth/authorize"
+EVE_TOKEN_URL = "https://login.eveonline.com/v2/oauth/token"
+EVE_VERIFY_URL = "https://esi.evetech.net/verify/"
 
 db.init_app(app)
 
@@ -25,3 +36,5 @@ with app.app_context():
 
 
 from app import routes
+
+socketio.run(app)
