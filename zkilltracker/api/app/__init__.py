61d258fc62356dd80ebb3cd7b995d7570097bef0
mandjevant <38689620+mandjevant@users.noreply.github.com>
2025-01-01 19:08:30 +0100

    Remove websocket, add flask login, add db threading

--- zkilltracker/api/app/__init__.py
+++ zkilltracker/api/app/__init__.py
@@ -2,25 +2,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 import uuid
-from flask_socketio import SocketIO
+import flask_login
 
 
 db = SQLAlchemy()
 app = Flask(__name__)
 db_name = "zkillboard_stats.db"
 app.secret_key = str(uuid.uuid4())
-socketio = SocketIO(app)
+login_manager = flask_login.LoginManager()
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_name
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_name}?check_same_thread=False"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 EVE_CLIENT_ID = environ.get("CLIENT_ID")
 EVE_CLIENT_SECRET = environ.get("SECRET_KEY")
 EVE_REDIRECT_URI = environ.get("CALLBACK_URL")
+OWNER_CHAR_ID = environ.get("OWNER_CHAR_ID")
 EVE_AUTH_URL = "https://login.eveonline.com/v2/oauth/authorize"
 EVE_TOKEN_URL = "https://login.eveonline.com/v2/oauth/token"
 EVE_VERIFY_URL = "https://esi.evetech.net/verify/"
 
 db.init_app(app)
+login_manager.init_app(app)
 
 with app.app_context():
     from app.models import (
@@ -38,5 +40,3 @@ with app.app_context():
 
 
 from app import routes
-
-socketio.run(app)
