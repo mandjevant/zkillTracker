0bd05e67e71af0aa9511566c7af375b5b2e068bf
mandjevant <38689620+mandjevant@users.noreply.github.com>
2025-01-02 12:03:10 +0100

    rework login view

--- zkilltracker/src/components/loginView.jsx
+++ zkilltracker/src/components/loginView.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { showNotification, cleanNotifications } from '@mantine/notifications';
-import { negaNotifProps, posiNotifProps } from './helpers';
+import { showNotification } from '@mantine/notifications';
+import { negaNotifProps } from './helpers';
 import { useAuth } from '../App';
 
 export default function LoginView() {
-  const [loggingIn, setLoggingIn] = useState(false);
-  const { isLoggedIn, setIsLoggedIn, setIsAdmin, setLoggedInCharName } = useAuth();
+  const [setLoggingIn] = useState(false);
+  const { isLoggedIn } = useAuth();
 
   if (isLoggedIn) {
     window.location.replace("http://localhost:3000/corporation")
@@ -18,14 +18,7 @@ export default function LoginView() {
         const authURL = response.data;
         setLoggingIn(true);
 
-        const handle = window.open(authURL);
-
-        if (!handle) {
-          showNotification({
-            message: "Failed to open login window.",
-            ...negaNotifProps
-          })
-        };
+        window.location.href = authURL;
       })
       .catch(error => {
         showNotification({
@@ -36,43 +29,6 @@ export default function LoginView() {
       })
   };
   
-  useEffect(() => {
-    if (isLoggedIn) {
-      return
-    }
-    let interval;
-    if (loggingIn) {
-      interval = setInterval(() => {
-        axios.get("/check_approval")
-          .then(response => {
-            if (response.data.status === "Done") {
-              setLoggingIn(false);
-              setIsLoggedIn(response.data.logged_in);
-              setIsAdmin(response.data.is_admin);
-              setLoggedInCharName(response.data.character_name);
-              cleanNotifications();
-              showNotification({
-                message: "Logged in successfully!",
-                ...posiNotifProps
-              });
-              clearInterval(interval);
-              window.location.replace("http://localhost:3000/corporation")
-            }
-          })
-          .catch(error => {
-            setLoggingIn(false);
-            cleanNotifications();
-            showNotification({
-              message: "Login unsuccessfull!",
-              ...negaNotifProps
-            });
-            clearInterval(interval);
-          });
-      }, 500);
-    }
-    return () => clearInterval(interval);
-  }, [loggingIn, isLoggedIn, setIsAdmin, setIsLoggedIn, setLoggedInCharName]);
-
   return (
     <div className="App">
       <div className="AppLogin">
